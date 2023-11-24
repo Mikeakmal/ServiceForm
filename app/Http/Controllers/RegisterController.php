004221ba902f999c00b95ce81a4cbf28cdbe4c3b
Mikeakmal <marinaakmal1805@gmail.com>
2023-11-24 08:46:06 +0700

    LAST10

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,31 +11,31 @@ class RegisterController extends Controller
 {
     public function index()
     {   
-        $data = [
-            'user' => Auth::user(), 
-        ];
+        // $data = [
+        //     'user' => Auth::user(), 
+        // ];
 
-        $register = '';
-        return view('register.index', compact('register')
-        );
+        // $register = '';
+        // return view('register.index', compact('register')
+        // );
     }
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => ['required', 'min:3', 'max:225', 'unique:users'],
-            'email' => 'required|email:dns|unique:users',
-            'password' => 'required|min:5|max:255'
-        ]);
+        // $validatedData = $request->validate([
+        //     'name' => ['required', 'min:3', 'max:225', 'unique:users'],
+        //     'email' => 'required|email:dns|unique:users',
+        //     'password' => 'required|min:5|max:255'
+        // ]);
     
-        // Periksa apakah alamat email sudah ada dalam basis data
-        $existingUser = User::where('email', $request->email)->first();
+        // // Periksa apakah alamat email sudah ada dalam basis data
+        // $existingUser = User::where('email', $request->email)->first();
 
-        // Jika alamat email belum ada, lanjutkan dengan penyimpanan
-        $validatedData['password'] = Hash::make($validatedData['password']);
-        User::create($validatedData);
+        // // Jika alamat email belum ada, lanjutkan dengan penyimpanan
+        // $validatedData['password'] = Hash::make($validatedData['password']);
+        // User::create($validatedData);
     
-        return redirect('/login')->with('success', 'Registrasi berhasil! Silakan login.');
+        // return redirect('/login')->with('success', 'Registrasi berhasil! Silakan login.');
 
         // dd('registrasi berhasil');
     }
